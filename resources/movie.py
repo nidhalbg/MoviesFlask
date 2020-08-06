57d35884f17e3de3519d39ab90800e537b4d8bff
Nidhal Ben Ghzaiel <nidhal@yooth-it.com>
2020-08-06 18:05:45 +0200

    flask_restfull

--- resources/movie.py
+++ resources/movie.py
@@ -1,33 +1,28 @@
-from flask import Blueprint, Response, request, jsonify
+from flask import Response, request, jsonify
 from database.models import Movie
+from flask_restful import Resource
 
-movies = Blueprint('movies', __name__)
 
-@movies.route('/movies')
-def get_movies():
-    movies = Movie.objects().to_json()
-    return Response(movies, mimetype="application/json", status=200)
+class MoviesApi(Resource):
+    def get(self):
+        movies = Movie.objects().to_json()
+        return Response(movies, mimetype="application/json", status=200)
 
-@movies.route('/movies', methods=['POST'])
-def add_movie():
-    body = request.get_json()
-    movie = Movie(**body).save()
-    return jsonify(movie), 200
+    def post(self):
+        body = request.get_json()
+        movie = Movie(**body).save().to_json()
+        return Response(movie, mimetype="application/json", status=200)
 
-@movies.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-    body = request.get_json()
-    #Movie.objects.get(id=id).update(**body)
-    movie = Movie.objects.get(id=id).update(**body)
+class MovieApi(Resource):
+    def put(self, id):
+        body = request.get_json()
+        movie = Movie.objects.get(id=id).update(**body)
+        return Response({"message": "success"}, mimetype="application/json", status=200)
+ 
+    def delete(self, id):
+        movie = Movie.objects.get(id=id).delete()
+        return '', 200
 
-    return jsonify({"message": "success", "movie": movie}), 200
-
-@movies.route('/movies/<id>', methods=['DELETE'])
-def delete_movie(id):
-    movie = Movie.objects.get(id=id).delete()
-    return jsonify({'done': 'done'}), 200
-
-@movies.route('/movies/<id>')
-def get_movie(id):
-    movie = Movie.objects.get(id=id).to_json()
-    return Response(movie, mimetype="application/json", status=200)
+    def get(self, id):
+        movies = Movie.objects.get(id=id).to_json()
+        return Response(movies, mimetype="application/json", status=200)
